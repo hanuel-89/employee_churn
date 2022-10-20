0466017e7cfa7bc5fa9ffbfe935640f4fab95c44
Emmanuel Folorunsho <55240019+hanuel-89@users.noreply.github.com>
2022-10-20 14:28:53 +0100

    complete load validation

--- apps/ingestion/load_validate.py
+++ apps/ingestion/load_validate.py
@@ -7,7 +7,7 @@ import os
 
 import pandas as pd
 
-from apps.database.database_operation import DatabaseOperation
+from apps.database.database_operation import DataBaseOperation as dbOperation
 from apps.core.logger import Logger
 
 class LoadValidate:
@@ -205,6 +205,132 @@ class LoadValidate:
                     if f not in os.listdir(dest):
                         shutil.move(source + f, dest)
             self.logger.info('End of archiving old validated files')
+
+            # Archive old result files
+            self.logger.info('Start of archiving old result files')
+            source = f'{self.data_path}_results/'
+            if os.path.isdir(source):
+                path = f'{self.data_path}_archive'
+                if not os.path.isdir(path):
+                    os.makedirs(path)
+                dest = f'{path}/results_{str(date)}_{str(time)}'
+                files = os.listdir(source)
+                for f in files:
+                    if not os.path.isdir(dest):
+                        os.makedirs(dest)
+                    if f not in os.listdir(dest):
+                        shutil.move(source + f, dest)
+            self.logger.info('End of archiving old result files')
+        except Exception as e:
+            self.logger.exception(f'Error occured while archiving old rejected file: {e}')
+            raise e
+
+
+    def move_processed_files(self):
+        """
+        Method to move processed files to a dedicated processed folder
+
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        try:
+            self.logger.info('----- Start of moving processed files -----')
+            for file in listdir(self.data_path):
+                shutil.move(f'{self.data_path}/{file}', f'{self.data_path}_processed')
+                self.logger.info(f'Moved the processed file: {file}')
+            self.logger.info('----- End of moving processed files -----')
+        except Exception as e:
+            self.logger.exception(f'Error raised while moving processed files: {e}')
+            raise e
+
+
+    def validate_trainset(self):
+        """
+        Method to validate the training dataset
+
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        try:
+            self.logger.info('----- Start of loading, validating, and transforming the training dataset -----')
+            # Archive the old files
+            self.archive_old_files()
+            # Extract values from the training schema
+            column_names, number_of_columns = self.values_from_schema('schema_train')
+            # Validate the column length in the file
+            self.validate_column_length(number_of_columns)
+            # Validate missing values in the column
+            self.validate_missing_values()
+            # Replace missing values (if any) with NULL
+            self.replace_missing_values()
+            # Create a database and open a connection. Create a table with the given column names
+            self.dbOperation.create_table('training', 'training_raw_data_t', column_names)
+            # Insert CSV files into the table
+            self.dbOperation.insert_data('training', 'training_raw_data_t')
+            # Export data in table to CSV file
+            self.dbOperation.export_csv('training', 'training_raw_data_t')
+            # Move the processed files
+            self.move_processed_files()
+
+            self.logger.info('---- End of data load, validation, and transformation -----')
+        except Exception as e:
+            self.logger.exception(f'Error occurred while validating the training dataset: {e}')
+            raise e
+
+
+    def validate_predictset(self):
+        """
+        Method to validate the prediction dataset
+
+        Args:
+            None
+        Returns:
+            None
+        """
+
+        try:
+            self.logger.info('----- Start of loading, validating, and transforming the prediction dataset -----')
+            # Archive the old files
+            self.archive_old_files()
+            # Extract values from the training schema
+            column_names, number_of_columns = self.values_from_schema('schema_train')
+            # Validate the column length in the file
+            self.validate_column_length(number_of_columns)
+            # Validate missing values in the column
+            self.validate_missing_values()
+            # Replace missing values (if any) with NULL
+            self.replace_missing_values()
+            # Create a database and open a connection. Create a table with the given column names
+            self.dbOperation.create_table('prediction', 'predictiong_raw_data_t', column_names)
+            # Insert CSV files into the table
+            self.dbOperation.insert_data('predictiong', 'predictiong_raw_data_t')
+            # Export data in table to CSV file
+            self.dbOperation.export_csv('predictiong', 'predictiong_raw_data_t')
+            # Move the processed files
+            self.move_processed_files()
+
+            self.logger.info('---- End of data load, validation, and transformation -----')
+        except Exception as e:
+            self.logger.exception(f'Error occurred while validating the predictiong dataset: {e}')
+            raise e
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
